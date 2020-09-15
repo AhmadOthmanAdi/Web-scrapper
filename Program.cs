2969cbd54bd7b15287237b64100b2cf9dbf14685
TonieHaux <Tonie@tonie.com>
2020-09-15 20:01:17 +0200

    This is a Web Scrapper for Search and Collection

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+
+namespace Scraper
+{
+    class Program
+    {
+        static ScrapingBrowser _browser = new ScrapingBrowser();
+
+        static void Main(string[] args)
+        {
+
+            GetHtml("https://newyork.craigslist.org/d/computer-gigs/search/cpg");
+
+        }
+        static HtmlNode GetHtml(string url)
+        {
+            WebPage webpage = _browser.NavigateToPage(new Uri(url));
+            return webpage.Html;
+        }
+    }
+}
